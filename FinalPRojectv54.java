f5b5dc1c4264eb9f5d80a108959e7d1ce3d0bdc7
RobinRagas123 <raphaelragas3@gmail.com>
2024-11-27 08:07:54 +0800

    Add files via upload

---
+++ FinalPRojectv54.java
@@ -0,0 +1,82 @@
+
+package finalprojectv.pkg5.pkg4;
+
+import java.util.Scanner;
+
+public class FinalPRojectv54 {
+    
+    //Legend:
+    //prin   -> principal amount
+    //monpay -> monthly payment
+    //nop    -> number of payments
+    //bal    -> balance
+    //intpay -> interest pay
+    //i      -> interest rate
+    //pripay -> principal pay
+    
+    public static void main(String[] args) {
+      Scanner sc = new Scanner  (System.in);
+      int main = 0;
+      do {
+          
+           System.out.println(" ~ Amortization Schedule Program ~ ");
+           System.out.println("Select a mode: ");
+           System.out.println("1: Balance");
+           System.out.println("2: Interest Paid");
+           System.out.println("3: Principal Pay");
+           System.out.println();
+           int mode = sc.nextInt();
+           System.out.println();
+            switch (mode) {
+              case 1 ->                   {
+                  
+                      System.out.println("Enter Principal amount: ");
+                      double prin = sc.nextDouble();
+                      System.out.println("Enter Monthly payment: ");
+                      double monpay = sc.nextDouble();
+                      System.out.println("Enter number of payments: ");
+                      int nop = sc.nextInt();
+                      double bal = prin - (monpay * nop);
+                      System.out.println("Balance is: " + bal);
+                      System.out.println();
+                      System.out.println("Try again.");
+                      System.out.println();
+                      
+                  }
+              case 2 ->                   {
+                  
+                      System.out.println ("Enter balance: ");
+                      double bal = sc.nextDouble();
+                      System.out.println("Enter monthly interest rate: ");
+                      double i = sc.nextDouble();
+                      double intpay = bal * i;
+                      System.out.println("Interest pay: " + intpay);
+                      System.out.println();
+                      System.out.println("Try again.");
+                      System.out.println();
+                      
+                  }
+              case 3 ->                   {
+                  
+                      System.out.println("Enter Monthly payment: ");
+                      double monpay = sc.nextDouble();
+                      System.out.println("Enter Interest paid: ");
+                      double intpay = sc.nextDouble();
+                      double pripay = monpay - intpay;
+                      System.out.println("Principal Pay: " + pripay);
+                      System.out.println();
+                      System.out.println("Try again.");
+                      System.out.println();
+                  }
+              default -> {
+                  
+                  System.out.println("Invalid Mode, Try Again.");
+                  System.out.println();
+                  
+              }
+          }
+      }
+      while (main == 0);
+    }
+    
+}
